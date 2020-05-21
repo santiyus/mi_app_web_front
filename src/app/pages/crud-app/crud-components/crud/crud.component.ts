4b413a7ba99218135d7e9c864becd4c9ef476f5d
santiyus <milsantus@gmail.com>
2020-05-21 18:21:33 +0200

    maqueta crud hecha a falta de la tabla

--- src/app/pages/crud-app/crud-components/crud/crud.component.ts
+++ src/app/pages/crud-app/crud-components/crud/crud.component.ts
@@ -11,48 +11,148 @@ import { Router } from '@angular/router';
 export class CrudComponent implements OnInit {
   token
   text
-  radioSelect = 1
+  radioSelect = 3
   radioBuscar = 'id'
+  disableInsert = true
 
-  errorGlobal=''
+  errorGlobal = ''
   resptBuscar = '';
   resptBorrar = '';
+
+  //tabla
+  columnas = []
 
   //mensajes de error
   msgError = 'error'
   msgNoFound = 'error-no-found'
+
+  //forms
+  formBuscar = this.fb.group({
+    resp: ['', Validators.required]
+  });
+
+  formInsert = this.fb.group({
+    nombre: ['', Validators.required],
+    apellidos: ['', Validators.required],
+    email: ['', [Validators.required, Validators.email]],
+    telefono: ['', [Validators.required, Validators.pattern('(6|7)[ -]*([0-9][ -]*){8}')]]
+  });
+
+  formEditar = this.fb.group({
+    nombre: [{value:'', disabled:true}, Validators.required],
+    apellidos: [{value:'', disabled:true}, Validators.required],
+    email: [{value:'', disabled:true}, [Validators.required, Validators.email]],
+    telefono: [{value:'', disabled:true}, [Validators.required, Validators.pattern('(6|7)[ -]*([0-9][ -]*){8}')]]
+  });
+
+  formBorrar = this.fb.group({
+    id: ['', Validators.required]
+  });
+
+
 
   constructor(private fb: FormBuilder, private router: Router) {
     this.text = textos
   }
 
   ngOnInit(): void {
-    this.token='llamar servicio que devuelve token'
+    this.token = 'llamar servicio que devuelve token'
+    this.columnas = this.text.crud.tabla
+    // this.disabledInsert()
   }
 
-  // OPCION BUSCAR
+  //Radio menu   
   changeRadio(event: any) {
-    this.limpiarErrores ()
+    this.resetFomrs()
+    this.limpiarErrores()
     this.radioSelect = event.target.id
   }
+
+
+  //Buscar
   changeRadioBuscar(event: any) {
-    this.limpiarErrores ()
+    this.limpiarErrores()
     this.radioBuscar = event.target.id
   }
 
-  buscarId() {
-    this.errorGlobal = this.msgError
+  onSubmitBuscar() {
+    console.log(this.formBuscar)
+
+    if (this.formBuscar.valid) {
+      const value = this.formBuscar.value;
+      console.log(value);
+
+      if ( this.radioSelect === 1) { 
+        console.log('buscar')
+
+      } else if ( this.radioSelect === 3){
+        this.enabledInsert()
+        console.log('editar --> ' +this.disableInsert)
+      }
+    } else {
+      this.disabledInsert()
+      this.formBuscar.markAllAsTouched();
+    }
   }
 
-  buscarName() {
-    this.errorGlobal = this.msgNoFound
+
+  //insertar
+  onSubmitInsert() {
+    console.log(this.formInsert)
+
+    if (this.formInsert.valid) {
+      const value = this.formInsert.value;
+      console.log(value);
+    } else {
+      this.formInsert.markAllAsTouched();
+    }
   }
 
-  borrarId() {
+
+  //editar
+  onSubmitEditar() {
+    console.log(this.formEditar)
+
+    if (this.formEditar.valid) {
+      const value = this.formEditar.value;
+      console.log(value);
+    } else {
+      this.formEditar.markAllAsTouched();
+    }
   }
 
-  private limpiarErrores (){
-    this.errorGlobal=''
+  disabledInsert(){
+    this.disableInsert = true
+    this.formEditar.disable()    
+  }
+
+  enabledInsert(){
+    this.disableInsert = false
+    this.formEditar.enable()    
+  }
+
+
+  //borrar
+  onSubmitBorrar() {
+    console.log(this.formBorrar)
+
+    if (this.formBorrar.valid) {
+      const value = this.formBorrar.value;
+      console.log(value);
+    } else {
+      this.formBorrar.markAllAsTouched();
+    }
+  }
+
+  private resetFomrs(){
+    this.formBuscar.reset()
+    this.formInsert.reset()
+    this.formEditar.reset()
+    this.formBorrar.reset()
+  }
+
+  private limpiarErrores() {
+    this.errorGlobal = ''
   }
 
 
